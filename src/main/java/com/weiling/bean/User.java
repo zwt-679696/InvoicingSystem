92b3aaebf17095f616288fc23177c9d010d9c162
wanghuaipeng666 <1055665333@qq.com>
2019-05-13 18:01:23 +0800

    添加日期

--- src/main/java/com/weiling/bean/User.java
+++ src/main/java/com/weiling/bean/User.java
@@ -1,9 +1,12 @@
 package com.weiling.bean;
 
+import java.util.Date;
+
 public class User {
 	private Integer id;
 	private String name;
 	private String password;
 	private Integer age;
+	private Date riqi;
 
 }
